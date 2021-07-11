7f91feabf98d62c2a2af8a7e0f7c27d2886ce771
ducpm151061 <duc.pm151061@sis.hust.edu.vn>
2021-07-11 16:18:59 +0700

    nokia 5110 lcd

---
+++ nokia-5110/main.h
@@ -0,0 +1,11 @@
+#include "pico/stdlib.h"
+
+#define SPI_PORT spi0
+
+// #define LCD_MISO 4
+#define LCD_CS   5
+#define LCD_SCK  6
+#define LCD_MOSI 7
+#define LCD_RST  8
+#define LCD_DC   4
+#define LCD_BL   28
