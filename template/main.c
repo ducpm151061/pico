e90f177035d9b7e0b81a6987ab07557b32a0bc69
ducpm151061 <duc.pm151061@sis.hust.edu.vn>
2021-07-12 21:45:50 +0700

    fix template

--- template/main.c
+++ template/main.c
@@ -7,8 +7,8 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-
-int main() {
+int main()
+{
 #ifndef PICO_DEFAULT_LED_PIN
 #warning blink example requires a board with a regular LED
 #else
@@ -16,11 +16,12 @@ int main() {
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        printf("LED ON");
+    while (true)
+    {
+        printf("LED ON\r\n");
         gpio_put(LED_PIN, 1);
         sleep_ms(250);
-        printf("LED OFF");
+        printf("LED OFF\r\n");
         gpio_put(LED_PIN, 0);
         sleep_ms(250);
     }
